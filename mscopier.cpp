666c286a893bb1f137907d75416ace74f984535a
qianyl7 <s4112793@student.rmit.edu.au>
2025-08-26 21:38:44 +1000

    validate command line arguments and get n

--- mscopier.cpp
+++ mscopier.cpp
@@ -1,27 +1,49 @@
-#include <pthread.h> 
-#include <stdio.h>
-#include <stdlib.h> 
 #include <filesystem>
+#include <iostream>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 using std::filesystem::path;
 
 void *CopyFile() {
-    path destinationFolder = "destination_dir";
-    pthread_exit(NULL); 
+  path destinationFolder = "destination_dir";
+  pthread_exit(NULL);
 }
 
 int main(int argc, char *argv[]) {
-    int numOfThreads = 0;
+  // Check the number of arguments
+  if (argc != 4) {
+    return 1;
+  }
 
-    int threadReturnValue;
+  // Store command line arguments
+  std::string num = argv[1];
+  std::string source = argv[2];
+  std::string destination = argv[3];
 
-    // Store the Thread Identifiers
-    pthread_t threads[numOfThreads];
-
-    for (int k = 0; k < numOfThreads; k++) {
-        threadReturnValue = pthread_create(&threads[k], NULL, CopyFile, (void *)t);
+  int numOfThreads = 0;
+  // Convert n to an integer
+  try {
+    numOfThreads = std::stoi(num);
+    if (numOfThreads < 2 || numOfThreads > 10) {
+      std::cerr << "n must be between 2 and 10." << std::endl;
+      return 1;
     }
+  } catch (...) {
+    std::cerr << "n must be an integer between 2 and 10." << std::endl;
+    return 1;
+  }
 
-    // Terminate the Calling Thread
-    pthread_exit(NULL);
+  int threadReturnValue;
+
+  // Store the Thread Identifiers
+  pthread_t threads[numOfThreads];
+
+  for (int k = 0; k < numOfThreads; k++) {
+    threadReturnValue = pthread_create(&threads[k], NULL, CopyFile, (void *)t);
+  }
+
+  // Terminate the Calling Thread
+  pthread_exit(NULL);
 }
