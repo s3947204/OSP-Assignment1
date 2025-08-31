4bd43d45beef085b1aa1d98a10f8d1062a1b6966
qianyl7 <s4112793@student.rmit.edu.au>
2025-08-31 15:06:52 +1000

    added comment

--- mscopier.cpp
+++ mscopier.cpp
@@ -1,23 +1,19 @@
 
+#include <fstream>
 #include <iostream>
 #include <pthread.h>
+#include <stdexcept>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string>
-#include <fstream>
 #include <sys/stat.h>
-#include <stdexcept>
 
-
-
-void *CopyFile() {
- 
-  pthread_exit(NULL);
-}
+void *CopyFile() { pthread_exit(NULL); }
 
 int main(int argc, char *argv[]) {
   // Check the number of arguments
   if (argc != 4) {
+    std::cerr << "Missing arguments!" << std::endl;
     return 1;
   }
 
@@ -27,22 +23,24 @@ int main(int argc, char *argv[]) {
   std::string destination = argv[3];
 
   int numOfThreads = 0;
-  // Convert n to an integer
+  // Convert n to an integer and check if it's valid
   try {
     numOfThreads = std::stoi(num);
     if (numOfThreads < 2 || numOfThreads > 10) {
       std::cerr << "n must be between 2 and 10." << std::endl;
       return 1;
     }
-  } catch (const std::exception& e) {
+  } catch (const std::exception &e) {
     std::cerr << "n must be an integer between 2 and 10." << std::endl;
     return 1;
   }
+  // Check if source directory and destination directory exist
   struct stat sourcesb, destinationsb;
   if (stat(source.c_str(), &sourcesb) != 0 || !S_ISDIR(sourcesb.st_mode)) {
     std::cerr << "The source directory is not valid!" << std::endl;
     return 1;
-  } else if (stat(destination.c_str(), &destinationsb) != 0 || !S_ISDIR(destinationsb.st_mode)) {
+  } else if (stat(destination.c_str(), &destinationsb) != 0 ||
+             !S_ISDIR(destinationsb.st_mode)) {
     std::cerr << "The destination directory is not valid!" << std::endl;
     return 1;
   }
